1b8adad1972314501eb141eb5a7f9915d6fe0095
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2025-01-02 15:42:30 +0700

    123

---
+++ run.bat
@@ -0,0 +1,8 @@
+xcopy /Y "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common\src\game\dtos\BaseDTO.ts" "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common-release\src\bases\BaseDTO.ts"
+xcopy /Y "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common\src\game\controllers\BaseController.ts" "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common-release\src\bases\BaseController.ts"
+xcopy /Y "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common\src\game\services\BaseService.ts" "C:\Users\84357\OneDrive - HUCE\Desktop\mct-common-release\src\bases\BaseService.ts"
+
+// cd to common release
+// git add .
+// git commit -m "Update release"
+// git push
