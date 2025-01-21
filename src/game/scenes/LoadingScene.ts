8fa47e9ec5539d9a24a04068e7eab854d4889e8c
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2025-01-21 10:05:39 +0700

    add

--- src/game/scenes/LoadingScene.ts
+++ src/game/scenes/LoadingScene.ts
@@ -4,47 +4,31 @@ export default class LoadingScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('loading', 'assets/images/loader_spritesheet_1.png', {
-            frameWidth: 64,  
-            frameHeight: 64,
-            endFrame: 9
-        });
-
-        this.load.image('logo','assets/images/logo.png');
+   
+        this.load.image('logo','assets/images/MCT_logo.png');
+        this.load.image('title','assets/images/MCT_title.png');
+        this.load.image('text_loading','assets/images/MCT_text_loading.png');
+        this.load.image('effect','assets/images/MCT_effect.png');
+        this.load.image('expression','assets/images/MCT_expression.png');
     }
 
     create() {
-        const offsetTitle = -70;
-        const offsetLoading = 60;
-        const fontSizeTitle = '30px Arial';
-        const fontSizeLoading = '15px Arial';
 
-        this.add.text(this.scale.width / 2, this.scale.height / 2 + offsetTitle, 'Addition Scale', {
-            fontSize: fontSizeTitle,
+        this.add.image(0,0,'logo').setOrigin(0.5,0.5).setDisplaySize(95,75);
+
+        this.add.text(this.scale.width / 2 , this.scale.height / 2 - 80, 'EM HỌC TOÁN', {
+            fontSize: '30px Arial',
             fontStyle: "bold",
             color: 'black'
         }).setOrigin(0.5).setResolution(2);
-
-        this.add.text(this.scale.width / 2, this.scale.height / 2 + offsetLoading, 'LOADING', {
-            fontSize: fontSizeLoading,
-            color: 'black'
-        }).setOrigin(0.5).setResolution(2);
-
-        this.anims.create({
-            key: 'falling',     
-            frames: this.anims.generateFrameNumbers('loading', { start: 0, end: 9 }),
-            frameRate: 10,        
-            repeat: -1         
-        });
-
-        const loading = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'loading');
-        loading.play('falling');
+    
 
         this.cameras.main.once('camerafadeoutcomplete', () => {
             this.scene.launch('GamePlayScene');
         });
-
         // this.cameras.main.fadeOut(1000); 
+
+        
     }
 
     update() {
