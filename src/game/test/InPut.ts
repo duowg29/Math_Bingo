4a3e05da39b40b6eaac45a0e0576d431c2347fa4
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2025-02-12 17:31:47 +0700

    add

---
+++ src/game/test/InPut.ts
@@ -0,0 +1,19 @@
+
+export default class InPut{
+ add(a: number, b: number): number {
+    return a + b;
+}
+  
+ multiply(a: number, b: number): number {
+    return a * b;
+}
+
+myFunction(): void {
+    console.log("Hello")
+}
+
+
+}
+
+
+  
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import BaseController from "./game/controllers/BaseController";
+import InPut from "./game/test/InPut";
+
+export { BaseController,InPut};
