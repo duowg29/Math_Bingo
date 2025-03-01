c0fd9706228b13211532e1363ddc327992a172fa
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2025-03-01 12:03:13 +0700

    add liblaru into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+
 import BaseController from "./game/bases/BaseController";
 import BaseDTO from "./game/bases/BaseDTO";
 import BaseService from "./game/bases/BaseService";
 import BaseView from "./game/bases/BaseView";
 import LoadingScene from "./game/scenes/LoadingScene";
-// components - enums
 import { BlankTypeEnum } from "./game/math/components/enums/BlankTypeEnum";
 import { DrawDirectionEnum } from "./game/math/components/enums/DrawDirectionEnum";
 import { ElementTypeEnum } from "./game/math/components/enums/ElementTypeEnum";
@@ -11,18 +11,30 @@ import { EquationSideEnum } from "./game/math/components/enums/EquationSideEnum"
 import { ExpressionDisplayTypeEnum } from "./game/math/components/enums/ExpressionDisplayTypeEnum";
 import { OperatorTypeEnum } from "./game/math/components/enums/OperatorTypeEnum";
 import { OptionLayoutEnum } from "./game/math/components/enums/OptionLayoutEnum";
-// components - interface
 import { ICalculatable } from "./game/math/components/interfaces/ICalculatable";
 import { ICheckable } from "./game/math/components/interfaces/ICheckable";
 import { IComparable } from "./game/math/components/interfaces/IComparable";
 import { INumberable } from "./game/math/components/interfaces/INumberable";
-// components - models
+import { Cell } from "./game/math/systems/Cell";
+import { Grid } from "matter";
+import { ArrayUtils } from "./game/math/utils/ArrrayUtils";
+import { DepthUtil } from "./game/math/utils/DepthUtil";
+import { GridSystem } from "./game/math/systems/GridSystem";
+import { ExpressionHelper } from "./game/math/helpers/ExpressionHelper";
+import { TextStyleHelper } from "./game/math/helpers/TextStyleHelper";
+import { VSCH } from "./game/math/helpers/ViewSizeCalculatorHellper";
+import { NumberHelper } from "./game/math/helpers/NumberHelper";
+import { ElementFactory } from "./game/math/factories/ElementFactory";
+import { EquationFactory } from "./game/math/factories/EquationFactory";
+import { ExpressionFactory } from "./game/math/factories/ExpressionFactory";
+import { IFactory } from "./game/math/factories/IFactory";
+import { ProblemFactory } from "./game/math/factories/ProblemFactory";
 import { CheckResponse } from "./game/math/components/models/CheckResponse";
 import { ElementModel } from "./game/math/components/models/ElementModel";
-import { ExpressionNormalModel } from "./game/math/components/models/ExpressionNormalModel";
 import { EquationModel } from "./game/math/components/models/EquationModel";
 import { ExpressionBlankModel } from "./game/math/components/models/ExpressionBlankModel";
 import { ExpressionModel } from "./game/math/components/models/ExpressionModel";
+import { ExpressionNormalModel } from "./game/math/components/models/ExpressionNormalModel";
 import { NumberBlankModel } from "./game/math/components/models/NumberBlankModel";
 import { NumberModel } from "./game/math/components/models/NumberModel";
 import { NumberNormalModel } from "./game/math/components/models/NumberNormalModel";
@@ -30,62 +42,29 @@ import { OperatorModel } from "./game/math/components/models/OperatorModel";
 import { ProblemEquationModel } from "./game/math/components/models/ProblemEquationModel";
 import { ProblemEquationSelectAnswerMultipleModel } from "./game/math/components/models/ProblemEquationSelectAnswerMultipleModel";
 import { ProblemEquationSelectAnswerSingleModel } from "./game/math/components/models/ProblemEquationSelectAnswerSingleModel";
-import { ProblemEquationSelectAnswerSingleSlider } from "./game/math/components/models/ProblemEquationSelectAnswerSingleSliderModel";
 import { ProblemMatchThePairModel } from "./game/math/components/models/ProblemMatchThePairModel";
 import { ProblemModel } from "./game/math/components/models/ProblemModel";
+import { ProblemSequenceModel } from "./game/math/components/models/ProblemSequenceModel";
 import { SequenceModel } from "./game/math/components/models/SequenceModel";
 import { TextModel } from "./game/math/components/models/TextModel";
-import { ProblemSequenceModel } from "./game/math/components/models/ProblemSequenceModel";
+import yoTechSDK, { Config, GameConfig, GameInstance, ProductionInfo, Reward, SDKMessage, SKDParams, SystemInfo, UserQuest } from "./game/utils/yoTechSDK";
 
+
+export { BaseController, BaseDTO, BaseService, LoadingScene, BaseView };
+// component-enums
+export { BlankTypeEnum, DrawDirectionEnum, ElementTypeEnum, EquationSideEnum, ExpressionDisplayTypeEnum, OperatorTypeEnum, OptionLayoutEnum }
+// component - interfaces
+export type { ICalculatable, ICheckable, IComparable, INumberable }
+// component - models
+export { CheckResponse, ElementModel, EquationModel, ExpressionBlankModel,ExpressionModel, ExpressionNormalModel, NumberBlankModel, NumberModel, NumberNormalModel, OperatorModel, ProblemEquationModel, ProblemEquationSelectAnswerMultipleModel, ProblemEquationSelectAnswerSingleModel, ProblemMatchThePairModel, ProblemModel, ProblemSequenceModel, SequenceModel, TextModel  }
 // factories
-import { ElementFactory } from "./game/math/factories/ElementFactory";
-import { EquationFactory } from "./game/math/factories/EquationFactory";
-import { ExpressionFactory} from "./game/math/factories/ExpressionFactory";
-import { IFactory } from "./game/math/factories/IFactory";
-import { ProblemFactory } from "./game/math/factories/ProblemFactory";
-//utils
-import { ArrayUtils } from "./game/math/utils/ArrrayUtils";
-import { DepthUtil } from "./game/math/utils/DepthUtil";
+export { ElementFactory, EquationFactory, ExpressionFactory, ProblemFactory };
+export type { IFactory };
+// helpers
+export { ExpressionHelper, NumberHelper, TextStyleHelper, VSCH }
+// systems
+export { Cell, Grid, GridSystem}
+// utils
+export { ArrayUtils, DepthUtil}
 
-
-
-export {
-    // Base classes
-    BaseController, BaseDTO, BaseService, BaseView, LoadingScene,
-  
-    // Enums
-    BlankTypeEnum, DrawDirectionEnum, ElementTypeEnum, EquationSideEnum, ExpressionDisplayTypeEnum, OperatorTypeEnum, OptionLayoutEnum,
-  
-    // Models
-    CheckResponse, ElementModel, EquationModel, ExpressionBlankModel, ExpressionModel, ExpressionNormalModel, NumberBlankModel,
-    NumberModel, NumberNormalModel, OperatorModel, ProblemEquationModel, ProblemEquationSelectAnswerMultipleModel,
-    ProblemEquationSelectAnswerSingleModel, ProblemEquationSelectAnswerSingleSlider, ProblemMatchThePairModel, SequenceModel, ProblemModel, ProblemSequenceModel, TextModel,
-  
-    // View Models
-    // ElementViewModel, ElementViewModelCell, ElementViewModelXY,
-  
-    // Views
-    // ElementView, EquationOptionsView, EquationView, ExpressionOptionView, ExpressionView, NumberBlankView, NumberBlockView,
-    // NumberTextView, NumberView, OperatorView, ProblemEquationSelectAnswerSingleView, ProblemEquationSelectAnswerSingleViewNew,
-    // ProblemEquationView, TextView,
-  
-    // UI Views
-    // UIButtonView, UIProgressBarView, UITopBarView, UIView,
-  
-    // Factories
-    ElementFactory, EquationFactory, ExpressionFactory, ProblemFactory,
-  
-    // Helpers
-    // ExpressionHelper, NumberHelper, TextStyleHelper, VSCH,
-  
-    // Systems
-    // Cell, Grid, GridSystem,
-  
-    // Utils
-    ArrayUtils, DepthUtil
-  };
-  export type {IFactory}
-  
-  export type { ICalculatable, ICheckable, IComparable, INumberable };
-  
-  
+export type { SDKMessage, SKDParams, SystemInfo, ProductionInfo, Config, GameInstance, GameConfig, UserQuest, Reward, yoTechSDK}
